fb305b3de62e1b556dc0c1a39a89a9639d20b828
Md Riyazul Islam <121575277+Riyazul555@users.noreply.github.com>
2024-03-21 20:02:18 +0530

    Update Ford-Fulkerson.java

--- Ford-Fulkerson/Ford-Fulkerson.java
+++ Ford-Fulkerson/Ford-Fulkerson.java
@@ -1,8 +1,7 @@
 // This code was contributed by Riyazul555
 
-// Time Complexity = O(M * (V + E))
-// Space Complexity = O(V^2)
-
+// Time Complexity = O(f * (V + E))   where V is the total number of nodes in the graph, E is the total number of edges in the graph, f is the Maximum flow
+// Space Complexity = O(V^2)          where V is the total number of nodes in the graph, E is the total number of edges in the graph, f is the Maximum flow
 import java.io.*;
 import java.util.*;
 
