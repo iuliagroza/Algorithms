ebeda7fa8c97e2ec9aa560489289c2c85959eb00
Md Riyazul Islam <121575277+Riyazul555@users.noreply.github.com>
2024-03-21 17:00:20 +0530

    Fleury Algo added in Java (#70)
    * Fleury Algo added in Java
    
    * Required changes fixed
    
    * Update Fleury.java
    
    ---------
    
    Co-authored-by: Riyazul555 <riyazulislam2003@gmail.com>

---
+++ Fleury/Fleury.java
@@ -0,0 +1,64 @@
+// This code is contributed by Riyazul555
+
+// Time Complexity  =  O(V + E)  where V is the number of vertices and E is the number of edges.
+// Space Complexity =  O(V + E)  where V is the number of vertices and E is the number of edges.
+
+import java.io.*;
+import java.util.*;
+
+public class EulerPath {
+
+    static void euler(int nod, ArrayList<Pair<Integer, Integer>>[] G, ArrayList<Integer> e, BitSet viz) {
+        while (!G[nod].isEmpty()) {
+            Pair<Integer, Integer> it = G[nod].remove(G[nod].size() - 1);
+            if (!viz.get(it.second)) {
+                viz.set(it.second);
+                euler(it.first, G, e, viz);
+            }
+        }
+        e.add(nod);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader fin = new BufferedReader(new FileReader(args[0]));
+        BufferedWriter fout = new BufferedWriter(new FileWriter(args[1]));
+
+        StringTokenizer st = new StringTokenizer(fin.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        ArrayList<Pair<Integer, Integer>>[] G = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            G[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(fin.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            G[x].add(new Pair<>(y, i));
+            G[y].add(new Pair<>(x, i));
+        }
+
+        ArrayList<Integer> e = new ArrayList<>();
+        BitSet viz = new BitSet(m);
+        euler(1, G, e, viz);
+
+        for (int i = e.size() - 1; i > 0; i--) {
+            fout.write(e.get(i) + " ");
+        }
+
+        fin.close();
+        fout.close();
+    }
+
+    static class Pair<A, B> {
+        A first;
+        B second;
+
+        public Pair(A first, B second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+}
