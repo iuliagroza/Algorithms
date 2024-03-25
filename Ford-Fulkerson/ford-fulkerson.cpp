ed0d2c4bcdab800165eceec02b1ac18fe6a0bbb8
Md Riyazul Islam <121575277+Riyazul555@users.noreply.github.com>
2024-03-25 17:58:04 +0530

    Ford-Fulkerson added in C++ (#76)
    * Ford-Fulkerson added in C++
    
    * Ford Fulkerson Algo added in C++
    
    * Changes fixed
    
    * Changes fixed
    
    * Rename Ford-Fulkerson.cpp to ford-fulkerson.cpp
    
    ---------
    
    Co-authored-by: Riyazul555 <riyazulislam2003@gmail.com>

---
+++ Ford-Fulkerson/ford-fulkerson.cpp
@@ -0,0 +1,71 @@
+// This code was contributed by Riyazul555 and iuliagroza
+
+// Time Complexity = O(f * (V + E))   where V is the total number of nodes in the graph, E is the total number of edges in the graph, f is the Maximum flow
+// Space Complexity = O(V^2)          where V is the total number of nodes in the graph, E is the total number of edges in the graph, f is the Maximum flow
+#include <fstream>
+#include <vector>
+#include <queue>
+#define nMax 1005
+
+using namespace std;
+
+int C[nMax][nMax];
+
+int bfs(vector<int> G[nMax], int n, int dad[nMax])
+{
+    queue<int> Q;
+	Q.push(1);
+	while(!Q.empty())
+	{
+		int nod=Q.front();
+		Q.pop();
+		for(int i:G[nod])
+			if (!dad[i] && C[nod][i]>0)
+			{
+				dad[i]=nod;
+				Q.push(i);
+			}
+	}
+	return dad[n];
+}
+
+int main(int argc, char* argv[])
+{
+	ifstream fin(argv[1]);
+	ofstream fout(argv[2]);
+
+	int n, m;
+	fin >> n >> m;
+	vector<int> G[nMax];
+
+
+	for(int i=1; i<=m; i++)
+	{
+	    int x, y, c;
+		fin >> x >> y >> c;
+		x++;
+		y++;
+		C[x][y]=c;
+		G[x].push_back(y);
+		G[y].push_back(x);
+	}
+	int flow = 0, dad[nMax]={0};
+	while(bfs(G, n, dad))
+	{
+		for(auto i:G[n])
+		{
+			int Min=C[i][n];
+			for(int j=i; j!=1; j=dad[j])
+				Min=min(Min, C[dad[j]][j]);
+			for(int j=i; j!=1; j=dad[j])
+				C[dad[j]][j]-=Min, C[j][dad[j]]+=Min;
+
+			flow+=Min;
+			C[i][n]-=Min;
+			C[n][i]+=Min;
+		}
+		fill(dad, dad+n+1, 0);
+	}
+    fout << flow;
+	return 0;
+}
